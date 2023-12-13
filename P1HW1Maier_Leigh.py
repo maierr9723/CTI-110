90e379cdb05583e47386dd77a306f529459a6cd1
maierr9723 <153647710+maierr9723@users.noreply.github.com>
2023-12-13 15:09:00 -0500

    Add files via upload

---
+++ P1HW1Maier_Leigh.py
@@ -0,0 +1,20 @@
+#Leigh Maier
+#11/07/23
+#Using integers and math expressions
+
+#get input from user
+number1 = int(input("enter a whole number: "))
+
+
+print ("You entered: ", number1)
+
+print (number1, "squared is", number1**2)
+
+print ("And", number1, "cubed is", number1*number1*number1)
+
+
+number2 = int(input("enter a whole number: "))
+
+print (number1, "+", number2, "is", number1+number2 )
+
+print (number1, "*", number2, "is", number1*number2 )
