c415b40e86fb9e99a1004ae7a1cacc3d44f58819
maierr9723 <153647710+maierr9723@users.noreply.github.com>
2023-12-13 15:08:35 -0500

    Add files via upload

---
+++ P1lab1maier_leigh.py
@@ -0,0 +1,9 @@
+#Leigh Maier
+#11/07/23
+#Basic Input/Output
+
+#Get in put from user
+user_name = input("Please enter your name: ")
+
+#display output to user
+print ("Hello", user_name,"Welcome to CTI110!")
